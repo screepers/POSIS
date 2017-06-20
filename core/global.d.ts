9ea408585a619991c3f8f209114fd57c9e78142c
Adam Shumann <ashumann13@gmail.com>
2017-06-20 10:37:31 -0500

    Fixed global in global.d.ts

--- core/global.d.ts
+++ core/global.d.ts
@@ -1,4 +1,4 @@
-interface global {
+interface Global {
     // register this function before require()ing your POSIS program bundles; they can call this at the end of their source file to register themselves
     // name your processes' image names with initials preceding, like ANI/MyCoolPosisProgram (but the actual class name can be whatever you want)
     // if you have several programs that are logically grouped (a "bundle") you can pretend that we have a VFS: "ANI/MyBundle/BundledProgram1"
