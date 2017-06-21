838192801768361b044cf31e4f86de8da01eb50d
Adam Shumann <ashumann13@gmail.com>
2017-06-21 14:24:19 -0500

    added main.js contents

--- examples/main.ts
+++ examples/main.ts
@@ -3,3 +3,11 @@ import {} from "PosisTest";
 import {} from "ags.example";
 
 export { loop } from "kernel/entry";
+
+/*
+Contents for main.js
+module.exports = require('kernel')
+require('init')
+require('ags.example')
+require('PosisTest')
+*/
