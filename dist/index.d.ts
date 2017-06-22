824e9e7680ce01f66b753ffa72e4cc29ac41667d
Adam Shumann <ashumann13@gmail.com>
2017-06-22 15:17:24 -0500

    Updated IPosisSpawnExtension based on recent talks

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,15 +1,3 @@
-type PosisInterface = "baseKernel" | "spawn";
-
-interface Global {
-    // register this function before require()ing your POSIS program bundles; they can call this at the end of their source file to register themselves
-    // name your processes' image names with initials preceding, like ANI/MyCoolPosisProgram (but the actual class name can be whatever you want)
-    // if you have several programs that are logically grouped (a "bundle") you can pretend that we have a VFS: "ANI/MyBundle/BundledProgram1"
-    registerPosisProcess(imageName: string, constructor: new () => IPosisProcess): boolean;
-    // For querying extension interfaces (instead of tying ourselves to "levels")
-    queryPosisInterface(interfaceId: PosisInterface): IPosisExtension | undefined;
-}
-
-type PosisPID = string | number;
 interface IPosisExtension {}
 interface IPosisKernel extends IPosisExtension {
     startProcess(imageName: string, startContext: any): IPosisProcess | undefined;
@@ -37,11 +25,39 @@ interface IPosisProcess {
     log: IPosisLogger; // Logger 
     run(): void; // main function
 }
+type PosisInterface = "baseKernel" | "spawn";
+
+interface Global {
+    // register this function before require()ing your POSIS program bundles; they can call this at the end of their source file to register themselves
+    // name your processes' image names with initials preceding, like ANI/MyCoolPosisProgram (but the actual class name can be whatever you want)
+    // if you have several programs that are logically grouped (a "bundle") you can pretend that we have a VFS: "ANI/MyBundle/BundledProgram1"
+    registerPosisProcess(imageName: string, constructor: new () => IPosisProcess): boolean;
+    // For querying extension interfaces (instead of tying ourselves to "levels")
+    queryPosisInterface(interfaceId: PosisInterface): IPosisExtension | undefined;
+}
+
+type PosisPID = string | number;
+declare const enum EPosisSpawnStatus {
+  UNKNOWN = -2,
+  ERROR = -1,
+  QUEUED,
+  SPAWNING,
+  SPAWNED
+}
+
+// NOT FINAL, discussions still underway in slack #posis
+
+// process calls spawnCreep, checks status, if not ERROR, poll 
+// getStatus until status is SPAWNED (Handling ERROR if it happens),
+// then call getCreep to get the creep itself
+
 interface IPosisSpawnExtension {
     // Queues/Spawns the creep and returns an ID
-    spawnCreep(sourceRoom: string, targetRoom: string, body: string[], memory: any): string;
+    spawnCreep(rooms: string[], body: string[][], memory?: any, opts?: { priority?: number }): string;
     // Used to see if its been dropped from queue
-    isValid(id: string): boolean;
-    hasSpawned(id: string): boolean;
+    getStatus(id: string): {
+      status: EPosisSpawnStatus
+      message?:string
+    }
     getCreep(id: string): Creep | undefined;
 }
--- src/extensions/IPosisSpawnExtension.d.ts
+++ src/extensions/IPosisSpawnExtension.d.ts
@@ -1,8 +1,24 @@
+declare const enum EPosisSpawnStatus {
+  UNKNOWN = -2,
+  ERROR = -1,
+  QUEUED,
+  SPAWNING,
+  SPAWNED
+}
+
+// NOT FINAL, discussions still underway in slack #posis
+
+// process calls spawnCreep, checks status, if not ERROR, poll 
+// getStatus until status is SPAWNED (Handling ERROR if it happens),
+// then call getCreep to get the creep itself
+
 interface IPosisSpawnExtension {
     // Queues/Spawns the creep and returns an ID
-    spawnCreep(sourceRoom: string, targetRoom: string, body: string[], memory: any): string;
+    spawnCreep(rooms: string[], body: string[][], memory?: any, opts?: { priority?: number }): string;
     // Used to see if its been dropped from queue
-    isValid(id: string): boolean;
-    hasSpawned(id: string): boolean;
+    getStatus(id: string): {
+      status: EPosisSpawnStatus
+      message?:string
+    }
     getCreep(id: string): Creep | undefined;
 }
