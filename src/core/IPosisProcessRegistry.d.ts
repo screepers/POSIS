e1b34c48f5e7a173efb85c68825f275eefadc22b
ezolenko <zolenkoe@gmail.com>
2017-06-24 10:36:53 -0600

    - fix for registration

--- src/core/IPosisProcessRegistry.d.ts
+++ src/core/IPosisProcessRegistry.d.ts
@@ -1,5 +1,5 @@
 interface IPosisProcessRegistry {
 	// name your processes' image names with initials preceding, like ANI/MyCoolPosisProgram (but the actual class name can be whatever you want)
 	// if your bundle consists of several programs you can pretend that we have a VFS: "ANI/MyBundle/BundledProgram1"
-	register(imageName: string, constructor: new () => IPosisProcess): boolean;
+	register(imageName: string, constructor: new (context: IPosisProcessContext) => IPosisProcess): boolean;
 }
