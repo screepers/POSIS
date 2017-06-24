f35f7f87f7e3555020bdeeca6126b376169a0b45
ezolenko <zolenkoe@gmail.com>
2017-06-24 00:00:03 -0600

    - build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -7,7 +7,8 @@ type PosisInterfaces = {
 // Bundle for programs that are logically grouped
 interface IPosisBundle {
 	install(registry: IPosisProcessRegistry): void;
-}interface IPosisExtension {}
+}
+interface IPosisExtension {}
 interface IPosisKernel extends IPosisExtension {
     startProcess(imageName: string, startContext: any): IPosisProcess | undefined;
     // killProcess also kills all children of this process
@@ -36,18 +37,22 @@ interface IPosisProcessContext {
 }
 
 interface IPosisProcess {
-    run(context: IPosisProcessContext): void; // main function
+    // Main function, implement all process logic here. 
+    // Bundle: Don't write to context object, host will likely freeze it anyway. 
+    // Host: freeze the thing!
+    run(context: IPosisProcessContext): void; 
 }
 interface IPosisProcessRegistry {
 	// name your processes' image names with initials preceding, like ANI/MyCoolPosisProgram (but the actual class name can be whatever you want)
 	// if your bundle consists of several programs you can pretend that we have a VFS: "ANI/MyBundle/BundledProgram1"
 	register(imageName: string, constructor: new () => IPosisProcess): boolean;
-}declare const enum EPosisSpawnStatus {
-  UNKNOWN = -2,
-  ERROR = -1,
-  QUEUED,
-  SPAWNING,
-  SPAWNED
+}
+declare const enum EPosisSpawnStatus {
+    UNKNOWN = -2,
+    ERROR = -1,
+    QUEUED,
+    SPAWNING,
+    SPAWNED
 }
 
 // NOT FINAL, discussions still underway in slack #posis
@@ -61,8 +66,8 @@ interface IPosisSpawnExtension {
     spawnCreep(rooms: string[], body: string[][], memory?: any, opts?: { priority?: number }): string;
     // Used to see if its been dropped from queue
     getStatus(id: string): {
-      status: EPosisSpawnStatus
-      message?:string
+        status: EPosisSpawnStatus
+        message?: string
     }
     getCreep(id: string): Creep | undefined;
 }
