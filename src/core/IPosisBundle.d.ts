5b1589ce6d4798045264f0f26614896f199f299c
ezolenko <zolenkoe@gmail.com>
2017-06-24 00:11:12 -0600

    - comments

--- src/core/IPosisBundle.d.ts
+++ src/core/IPosisBundle.d.ts
@@ -2,6 +2,8 @@
 interface IPosisBundle {
 	install(registry: IPosisProcessRegistry): void;
 
+	// image name of root process in the bundle, if any
 	rootImageName?: string;
+	// default start context for root process, if any
 	defaultStartContext?: any;
 }
