55935d18f79a48435971cd22c8291ef2845cf1c7
Nikolay Kharitonov <Nikolay.Kharitonov@dataart.com>
2017-06-26 05:16:15 +0300

    Removed memory option Added pid option Added descriptions to options

--- src/extensions/IPosisSpawnExtension.d.ts
+++ src/extensions/IPosisSpawnExtension.d.ts
@@ -1,5 +1,4 @@
 declare const enum EPosisSpawnStatus {
-    UNKNOWN = -2,
     ERROR = -1,
     QUEUED,
     SPAWNING,
@@ -14,7 +13,22 @@ declare const enum EPosisSpawnStatus {
 
 interface IPosisSpawnExtension {
     // Queues/Spawns the creep and returns an ID
-    spawnCreep(rooms: string[], body: string[][], memory?: any, opts?: { priority?: number }): string;
+    spawnCreep(opts: { 
+        //   - 'rooms' are names of rooms associated with the creep being spawned.
+        //     Must contain at least one room. Host should select spawner based on its own logic
+        //     May contain additional rooms as a hints to host. Host may ignore hints
+        rooms: string[], 
+        //   - 'body' are body variants of the creep being spawned, al least one must be provided
+        //     Host must guarantee that the spawning creep will have one of provided bodies
+        //     Which body to spawn is up to host to select based on its own logic
+        body: string[][], 
+        //   - 'priority' is spawn priority in range -1000 (the highest) to 1000 (the lowest)
+        //     Used as a hint for host's logic. Host may (but not guarantee) spawn creeps in priority order
+        priority?: number, 
+        //   - 'pid' is id of the process which is spawned creep associated to
+        //     Used as a hint for host's logic. Host may (but not guarantee) consider currently spawned creeps
+        //     detached, may (but not guarantee) remove scheduled creeps from queue on process termination
+        pid?: PosisPID }): string;
     // Used to see if its been dropped from queue
     getStatus(id: string): {
         status: EPosisSpawnStatus
