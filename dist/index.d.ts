17bc9b23ba4da734f70bbaf40a4a4e6f617b5e52
ezolenko <zolenkoe@gmail.com>
2017-06-27 19:46:12 -0600

    - comments

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -55,10 +55,11 @@ interface IPosisProcessRegistry {
 	// if your bundle consists of several programs you can pretend that we have a VFS: "ANI/MyBundle/BundledProgram1"
 	register(imageName: string, constructor: new (context: IPosisProcessContext) => IPosisProcess): boolean;
 }
-
 interface IPosisSleepExtension {
+    // puts currently running process to sleep for a given number of ticks
     sleep(ticks: number): void;
-}declare const enum EPosisSpawnStatus {
+}
+declare const enum EPosisSpawnStatus {
     ERROR = -1,
     QUEUED,
     SPAWNING,
