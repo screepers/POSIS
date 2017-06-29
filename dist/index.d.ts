cb10bdd1695386103dd2526dc631830a78dac474
Adam Shumann <ashumann13@gmail.com>
2017-06-29 09:00:57 -0500

    Rebuilt index.d.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,3 @@
-type PosisPID = string | number;
-
-type PosisInterfaces = {
-	baseKernel: IPosisKernel;
-	spawn: IPosisSpawnExtension;
-}
 // Bundle for programs that are logically grouped
 interface IPosisBundle<IDefaultRootMemory> {
 	// host will call that once, possibly outside of main loop, registers all bundle processes here
@@ -15,7 +9,7 @@ interface IPosisBundle<IDefaultRootMemory> {
 }
 interface IPosisExtension {}
 interface IPosisKernel extends IPosisExtension {
-    startProcess(imageName: string, startContext: any): IPosisProcess | undefined;
+    startProcess(imageName: string, startContext: any): { pid: PosisPID; process: IPosisProcess } | undefined;
     // killProcess also kills all children of this process
     // note to the wise: probably absorb any calls to this that would wipe out your entire process tree.
     killProcess(pid: PosisPID): void;
@@ -56,8 +50,13 @@ interface IPosisProcessRegistry {
 	// if your bundle consists of several programs you can pretend that we have a VFS: "ANI/MyBundle/BundledProgram1"
 	register(imageName: string, constructor: new (context: IPosisProcessContext) => IPosisProcess): boolean;
 }
+type PosisPID = string | number;
+
+type PosisInterfaces = {
+	baseKernel: IPosisKernel;
+	spawn: IPosisSpawnExtension;
+}
 declare const enum EPosisSpawnStatus {
-    UNKNOWN = -2,
     ERROR = -1,
     QUEUED,
     SPAWNING,
@@ -72,7 +71,23 @@ declare const enum EPosisSpawnStatus {
 
 interface IPosisSpawnExtension {
     // Queues/Spawns the creep and returns an ID
-    spawnCreep(rooms: string[], body: string[][], opts?: { priority?: number }): string;
+    spawnCreep(opts: { 
+        //   - 'rooms' are names of rooms associated with the creep being spawned.
+        //     Must contain at least one room. Host should select spawner based on its own logic
+        //     May contain additional rooms as a hints to host. Host may ignore hints
+        rooms: string[], 
+        //   - 'body' are body variants of the creep being spawned, at least one must be provided
+        //     Host must guarantee that the spawning creep will have one of provided bodies
+        //     Which body to spawn is up to host to select based on its own logic
+        //     Body templates should be sorted in the order of diminishing desirability
+        body: string[][], 
+        //   - 'priority' is spawn priority in range -1000 (the highest) to 1000 (the lowest)
+        //     Used as a hint for host's logic. Host may (but not guarantee) spawn creeps in priority order
+        priority?: number, 
+        //   - 'pid' is id of the process which is spawned creep associated to
+        //     Used as a hint for host's logic. Host may (but not guarantee) consider currently spawned creeps
+        //     detached, may (but not guarantee) remove scheduled creeps from queue on process termination
+        pid?: PosisPID }): string;
     // Used to see if its been dropped from queue
     getStatus(id: string): {
         status: EPosisSpawnStatus
